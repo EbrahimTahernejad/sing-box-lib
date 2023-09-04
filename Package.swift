03b0cfac068101ff7c324e7153a18659b854ab0f
EbrahimTahernejad <EbrahimTahernejad@users.noreply.github.com>
2023-09-04 11:32:07 +0000

    Libbox Release 1.0.2

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/EbrahimTahernejad/sing-box-lib/releases/download/1.0.1/Libbox.xcframework.zip",
-      checksum: "c3e2d33edfc04fc4c70edb074b084627d990d157baa766ea2f3604152490570d"
+      url: "https://github.com/EbrahimTahernejad/sing-box-lib/releases/download/1.0.2/Libbox.xcframework.zip",
+      checksum: "fc6040738159d2bfda3e3474705c20979e2fbf003e3e0e41e59512ae590d6dfb"
     )
   ]
 )
